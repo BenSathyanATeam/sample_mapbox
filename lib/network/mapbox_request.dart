e73a85a4ca1c518e0ad322e657d6ff94687974d6
ben.sathyan <Brother123@321>
2023-12-06 15:43:50 +0530

    feat : Added rout lat lng collections to draw poly lines

--- lib/network/mapbox_request.dart
+++ lib/network/mapbox_request.dart
@@ -1,7 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:mapbox_gl/mapbox_gl.dart'  as mapBox;
 
 import 'exception_handler.dart';
 
@@ -11,9 +12,23 @@ String navType = 'driving';
 
 Dio _dio = Dio();
 
-Future getCyclingRouteUsingMapbox(LatLng source, LatLng destination) async {
+Future getCyclingRouteUsingMapbox(mapBox.LatLng source, mapBox.LatLng destination) async {
   String url =
-      '$baseUrl/$navType/${source.longitude},${source.latitude};${destination.longitude},${destination.latitude}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=$accessToken';
+      '$baseUrl/$navType/${source.longitude},${source.latitude};${destination.longitude},${destination
+      .latitude}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=$accessToken';
+  try {
+    _dio.options.contentType = Headers.jsonContentType;
+    final responseData = await _dio.get(url);
+    return responseData.data;
+  } catch (e) {
+    final errorMessage = DioExceptions.fromDioError(e as DioError).toString();
+    debugPrint(errorMessage);
+  }
+}
+Future getRouteListUsingMapbox(LatLng source, LatLng destination) async {
+  String url =
+      '$baseUrl/$navType/${source.longitude},${source.latitude};${destination.longitude},${destination
+      .latitude}?alternatives=true&continue_straight=true&geometries=geojson&language=en&overview=full&steps=true&access_token=$accessToken';
   try {
     _dio.options.contentType = Headers.jsonContentType;
     final responseData = await _dio.get(url);
