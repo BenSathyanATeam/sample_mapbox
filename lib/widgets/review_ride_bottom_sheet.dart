324e64f3868460bba03c21674370db32e003e9b1
ben.sathyan <Brother123@321>
2023-12-06 15:46:42 +0530

    feat : Changes made in the review button

--- lib/widgets/review_ride_bottom_sheet.dart
+++ lib/widgets/review_ride_bottom_sheet.dart
@@ -1,57 +1,42 @@
 import 'package:flutter/material.dart';
 import '../utils/shared_prefs.dart';
 
-Widget reviewRideBottomSheet(
-    BuildContext context, String distance, String dropOffTime) {
+Widget reviewRideBottomSheet(BuildContext context, String distance, String dropOffTime) {
   String sourceAddress = getSourceAndDestinationPlaceText('source');
   String destinationAddress = getSourceAndDestinationPlaceText('destination');
 
-  return Positioned(
-    bottom: 0,
-    child: SizedBox(
-      width: MediaQuery.of(context).size.width,
-      child: Card(
-        clipBehavior: Clip.antiAlias,
-        child: Padding(
-          padding: const EdgeInsets.all(15),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('$sourceAddress ➡ $destinationAddress',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleMedium
-                        ?.copyWith(color: Colors.indigo,fontSize: 15)),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: ListTile(
-                    tileColor: Colors.grey[200],
-                    leading: const Image(
-                        image: AssetImage('assets/splash_icon.png'),
-                        height: 50,
-                        width: 50),
-                    title: const Text('Start Ride',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold)),
-                    subtitle: Text('$distance km, $dropOffTime'),
-                    trailing: const Text("",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 18)),
-                  ),
+  return SizedBox(
+    width: 200,
+    child: Card(
+      color: Colors.black.withOpacity(.2),
+      clipBehavior: Clip.antiAlias,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 10,right: 10,top: 10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisSize: MainAxisSize.min, children: [
+          Text('$sourceAddress ➡ $destinationAddress',
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Colors.white, fontSize: 10)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: ListTile(
+              tileColor: Colors.black.withOpacity(.1),
+              leading: const Image(
+                image: AssetImage(
+                  'assets/splash_icon.png',
                 ),
-             /*   ElevatedButton(
-                    onPressed: () => Navigator.push(context,
-                        MaterialPageRoute(builder: (_) => const TurnByTurn())),
-                    style: ElevatedButton.styleFrom(
-                        padding: const EdgeInsets.all(20)),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Text('Start your premier ride now'),
-                        ])),*/
-              ]),
-        ),
+                height: 20,
+                width: 20,
+                color: Colors.white,
+              ),
+              title: const Text('Start Ride',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white)),
+              subtitle: Text(
+                '$distance km, $dropOffTime',
+                style: const TextStyle(color: Colors.white,fontSize: 12),
+              ),
+              trailing: const Text("", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 0, color: Colors.white)),
+            ),
+          ),
+        ]),
       ),
     ),
   );
